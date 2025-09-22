58263c889da78edc86f117ec23031a55fb18e6fa
nickonyi <nickoluoch1130@gmail.com>
2025-09-22 14:18:18 +0300

    some code for node js

--- app.js
+++
@@ -1,8 +0,0 @@
-const EventEmitter = require("node:events");
-const eventEmitter = new EventEmitter();
-
-eventEmitter.on("start", (start, end) => {
-  console.log(`started from ${start} to ${end}`);
-});
-
-eventEmitter.emit("start", 1, 24);
