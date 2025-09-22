68a4d3cb1dca045f7c1a0aa874ab53549879d56c
nickonyi <nickoluoch1130@gmail.com>
2025-09-22 14:33:22 +0300

    source code

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-const fs = require("fs/promises");
+const generateRandomNumber = require('./utils');
 
-const example = async () => {
-  const fileName = "./test.txt";
-  try {
-    const data = await fs.readFile(fileName, "utf-8");
-    console.log(data);
-    const content = "some content";
-    await fs.appendFile(fileName, content);
-    console.log("wrote some content!");
-    const newData = await fs.readFile(fileName, "utf-8");
-    console.log(newData);
-  } catch (error) {
-    console.log(error);
-  }
-};
+console.log(`Random Number:${generateRandomNumber()}`);
 
-example();
