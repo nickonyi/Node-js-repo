2cd987cc95cd703fbe10af66bdca0d80b8662ebd
nickonyi <nickoluoch1130@gmail.com>
2025-09-23 14:41:26 +0300

    request object

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const PORT = process.env.PORT;
 const server = http.createServer((req, res) => {
   // res.setHeader("Content-type", "text/html");
   //res.statusCode = 404;
+  console.log(req.url);
+  console.log(req.method);
 
   res.writeHead(200, { "content-type": "text/html" });
   res.end("<h1>Hellow World!!</h1>");
