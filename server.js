2da38615de0ef95263992ed2878521cd1b531a71
nickonyi <nickoluoch1130@gmail.com>
2025-09-23 14:25:41 +0300

    http module and create server

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
-import http from 'http'
+import http from "http";
+import { json } from "stream/consumers";
 
+const PORT = 8080;
+
+const server = http.createServer((req, res) => {
+  // res.setHeader("Content-type", "text/html");
+  //res.statusCode = 404;
+
+  res.writeHead(500, { "content-type": "application/json" });
+  res.end(JSON.stringify({ message: "server error" }));
+});
+
+server.listen(PORT, () => {
+  console.log(`listening at port:${PORT}`);
+});
