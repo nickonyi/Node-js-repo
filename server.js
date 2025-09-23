7b54090835729ecffc5437b5c23c4cca6f82928b
nickonyi <nickoluoch1130@gmail.com>
2025-09-23 14:38:00 +0300

    environment variables

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 import http from "http";
-import { json } from "stream/consumers";
 
-const PORT = 8080;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
   // res.setHeader("Content-type", "text/html");
   //res.statusCode = 404;
 
-  res.writeHead(500, { "content-type": "application/json" });
-  res.end(JSON.stringify({ message: "server error" }));
+  res.writeHead(200, { "content-type": "text/html" });
+  res.end("<h1>Hellow World!!</h1>");
 });
 
 server.listen(PORT, () => {
