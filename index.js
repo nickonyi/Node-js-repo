818770cb27a5d1fe4fe2788b3528ac5da470740d
nickonyi <nickoluoch1130@gmail.com>
2025-09-23 13:36:05 +0300

    different ways to export and install modules in nodejs

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
-import { getPosts } from "./postController.js";
+import  getPosts, {getPostsLength}  from "./postController.js";
 
 console.log(getPosts());
+console.log(getPostsLength());
+
 
 
 //const {generateRandomNumber,celciousToFahrenheit }= require('./utils');
