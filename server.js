8fb65132516300d3b579bd97466a1bd9c686833b
nickonyi <nickoluoch1130@gmail.com>
2025-09-24 13:00:46 +0300

    request header

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ import http from "http";
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  // res.setHeader("Content-type", "text/html");
-  //res.statusCode = 404;
-  console.log(req.url);
-  console.log(req.method);
-
-  res.writeHead(200, { "content-type": "text/html" });
-  res.end("<h1>Hellow World!!</h1>");
+  if (req.url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end("<h1>Homepage</h1>");
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end("<h1>About</h1>");
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.end("<h1>Not found!</h1>");
+  }
 });
 
 server.listen(PORT, () => {
