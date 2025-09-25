1c76544c03146775b7c18d3d065705e990f7b3f1
nickonyi <nickoluoch1130@gmail.com>
2025-09-25 13:42:05 +0300

    building a simple API

--- server2.js
+++ server2.js
@@ -25,8 +25,22 @@ const server = createServer((req, res) => {
     res.setHeader("Content-Type", "application/json");
     res.write(JSON.stringify(users));
     res.end();
+  } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
+    const id = req.url.split("/")[3];
+    const user = users.find((user) => user.id === parseInt(id));
+    if (user) {
+      res.setHeader("Content-Type", "application/json");
+      res.write(JSON.stringify(user));
+      res.end();
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.statusCode = 404;
+      res.write(JSON.stringify({ message: "user not found!" }));
+      res.end();
+    }
   } else {
     res.setHeader("Content-Type", "application/json");
+    res.statusCode = 404;
     res.write(JSON.stringify({ message: "Route not found!" }));
     res.end();
   }
