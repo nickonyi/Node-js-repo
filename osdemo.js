a3d1426b1279f4ab427ff1f6ff393504a555d222
nickonyi <nickoluoch1130@gmail.com>
2025-10-04 23:28:16 +0300

    node js

--- osdemo.js
+++ osdemo.js
@@ -1,3 +1,13 @@
 import os from "os";
 
 console.log(os.userInfo());
+
+//total mem
+console.log(os.totalmem());
+
+//free mem
+console.log(os.freemem());
+
+//cpus
+console.log(os.cpus().length);
+console.log("yellow");
