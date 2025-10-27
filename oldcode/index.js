1802599c3091f5ca162c6abc895d8da2861104ca
nickonyi <nickoluoch1130@gmail.com>
2025-10-27 17:16:58 +0300

    new code

---
+++ oldcode/index.js
@@ -0,0 +1,14 @@
+
+import  getPosts, {getPostsLength}  from "./postController.js";
+
+console.log(getPosts());
+console.log(getPostsLength());
+
+
+
+//const {generateRandomNumber,celciousToFahrenheit }= require('./utils');
+
+//console.log(`Random Number:${generateRandomNumber()}`);
+//console.log(`Celcious to Fahrenheit: ${celciousToFahrenheit(20)}`);
+
+
