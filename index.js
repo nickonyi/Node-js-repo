ad910d433eac5158186fdde5a9add0d3038c5cbf
nickonyi <nickoluoch1130@gmail.com>
2025-11-06 20:21:40 +0300

    week 1 js

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-
-import  getPosts, {getPostsLength}  from "./postController.js";
+import getPosts, { getPostsLength } from "./postController.js";
 
 console.log(getPosts());
 console.log(getPostsLength());
-
-
-
-//const {generateRandomNumber,celciousToFahrenheit }= require('./utils');
+console.log(VIDEO_URL);
+const {generateRandomNumber,celciousToFahrenheit }= require('./utils');
 
 //console.log(`Random Number:${generateRandomNumber()}`);
 //console.log(`Celcious to Fahrenheit: ${celciousToFahrenheit(20)}`);
-
-
