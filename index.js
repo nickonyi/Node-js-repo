96d03d2da7ac384963c823d82f4f1a92eae8bf1e
nickonyi <nickoluoch1130@gmail.com>
2025-12-08 17:28:27 +0300

    code for main

--- index.js
+++
@@ -1,9 +0,0 @@
-import getPosts, { getPostsLength } from "./postController.js";
-
-console.log(getPosts());
-console.log(getPostsLength());
-console.log(VIDEO_URL);
-const {generateRandomNumber,celciousToFahrenheit }= require('./utils');
-
-//console.log(`Random Number:${generateRandomNumber()}`);
-//console.log(`Celcious to Fahrenheit: ${celciousToFahrenheit(20)}`);
