bf52eff1a42ab08c7fe6208f46d205ac77c1bedc
nickonyi <nickoluoch1130@gmail.com>
2025-12-10 13:41:14 +0300

    creating a web server using node js

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const http = require("http");
 const PORT = 3000;
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  const { method, url, headers } = req;
+
+  if (method === "GET" && url === "/cats") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    const responseBody = {
+      headers,
+      method,
+      url,
+      body: ["Mrs. Meowsers", "Hairball", "Jerk"],
+    };
+    res.write(JSON.stringify(responseBody));
+    res.end();
+  }
+});
 
 server.listen(PORT, (err) => {
   if (err) console.log(err);
