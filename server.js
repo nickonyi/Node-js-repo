42c449a380d89b3f74a07a37ad3cfd600f3af5a3
nickonyi <nickoluoch1130@gmail.com>
2025-12-13 16:22:03 +0300

    importing and exporting using the module syntax

--- server.js
+++ server.js
@@ -5,10 +5,25 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello world</h1>");
+  try {
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>Homepage</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>about page</h1>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>Not found</h1>");
+      }
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("Server error");
+  }
 });
 
 server.listen(PORT, () => {
