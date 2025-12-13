46041ff7d63e4dbccc40848694e186e0b6c867cf
nickonyi <nickoluoch1130@gmail.com>
2025-12-13 18:12:14 +0300

    building a simple api

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
-import { getPosts } from "./postController.js";
+import http from "http";
+const PORT = process.env.PORT;
 
-console.log(getPosts());
+const users = [
+  { id: 1, name: "John doe" },
+  { id: 2, name: "Jane doe" },
+  { id: 3, name: "Jenny doe" },
+];
 
-//const { generateRandomNum, celciousToFarenheight } = require("./utils");
-//console.log(`Random number: ${generateRandomNum()}`);
-//console.log(`Celcious to Farenheight: ${celciousToFarenheight(50)}`);
+const server = http.createServer((req, res) => {
+  if (req.url === "/api/users" && req.method === "GET") {
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify(users));
+    res.end();
+  } else if (req.url.match(/\/api\/users\/([0-9]+)/)) {
+    const id = req.url.split("/")[3];
+    const user = users.find((user) => user.id === parseInt(id));
+    if (user) {
+      res.setHeader("Content-Type", "application/json");
+      res.write(JSON.stringify(user));
+      res.end();
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.statusCode = 404;
+      res.write(JSON.stringify({ message: "user not found" }));
+      res.end();
+    }
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 404;
+    res.write(JSON.stringify({ message: "route not found" }));
+    res.end();
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening at port:${PORT}`);
+});
