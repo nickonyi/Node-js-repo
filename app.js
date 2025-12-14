94c0383598434bb770ceb6f2dccffc7bd8adaae5
nickonyi <nickoluoch1130@gmail.com>
2025-12-14 16:15:33 +0300

    cleaning up the data with middleware

--- app.js
+++ app.js
@@ -6,31 +6,40 @@ const users = [
   { id: 2, name: "Jane doe" },
   { id: 3, name: "Jenny doe" },
 ];
-
+//logger middleware
+const logger = (req, res, next) => {
+  console.log(`${req.method}  ${req.url}`);
+  next();
+};
 const server = http.createServer((req, res) => {
-  if (req.url === "/api/users" && req.method === "GET") {
-    res.setHeader("Content-Type", "application/json");
-    res.write(JSON.stringify(users));
-    res.end();
-  } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
-    const id = req.url.split("/")[3];
-    const user = users.find((user) => user.id === parseInt(id));
-    if (user) {
+  logger(req, res, () => {
+    if (req.url === "/api/users" && req.method === "GET") {
       res.setHeader("Content-Type", "application/json");
-      res.write(JSON.stringify(user));
+      res.write(JSON.stringify(users));
       res.end();
+    } else if (
+      req.url.match(/\/api\/users\/([0-9]+)/) &&
+      req.method === "GET"
+    ) {
+      const id = req.url.split("/")[3];
+      const user = users.find((user) => user.id === parseInt(id));
+      if (user) {
+        res.setHeader("Content-Type", "application/json");
+        res.write(JSON.stringify(user));
+        res.end();
+      } else {
+        res.setHeader("Content-Type", "application/json");
+        res.statusCode = 404;
+        res.write(JSON.stringify({ message: "user not found" }));
+        res.end();
+      }
     } else {
       res.setHeader("Content-Type", "application/json");
       res.statusCode = 404;
-      res.write(JSON.stringify({ message: "user not found" }));
+      res.write(JSON.stringify({ message: "route not found" }));
       res.end();
     }
-  } else {
-    res.setHeader("Content-Type", "application/json");
-    res.statusCode = 404;
-    res.write(JSON.stringify({ message: "route not found" }));
-    res.end();
-  }
+  });
 });
 
 server.listen(PORT, () => {
