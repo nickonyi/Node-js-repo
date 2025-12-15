24ee33d5a0a646035cd0cf65215513d5b6a853b0
nickonyi <nickoluoch1130@gmail.com>
2025-12-15 13:29:40 +0300

    url module

--- urlDemo.js
+++ urlDemo.js
@@ -1,1 +1,15 @@
 import url from "url";
+
+const urlString = "https:/www.google.com/search?q=hello+world";
+
+const urlObj = new URL(urlString);
+console.log(urlObj);
+
+//format()
+
+const params = new URLSearchParams(urlObj.search);
+
+params.append("limit", "5");
+console.log(params.get("q"));
+console.log(params);
+console.log(url.format(urlObj));
