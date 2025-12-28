b2ebaba71fdee069e236b2a05ff3c8b3e6573096
nickonyi <nickoluoch1130@gmail.com>
2025-12-28 15:28:04 +0300

    code

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
 import { body, validationResult } from "express-validator";
-
-function bonusTime(salary, bonus) {
-  // your code here
-  if (bonus) {
-    salary = salary * 10;
-    return `£${salary}`;
-  }
-  return salary;
-}
