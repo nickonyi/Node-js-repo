cd1c006427de4f08cb0df67e4249bc856f7615c1
nickonyi <nickoluoch1130@gmail.com>
2025-12-28 16:25:38 +0300

    form validation

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-import { body, validationResult } from "express-validator";
+import express from "express";
+const app = express();
+import url from "url";
+import path from "node:path";
+import { userRouter } from "./routes/userRouter.js";
+
+const __fileName = url.fileURLToPath(import.meta.url);
+const __dirName = path.dirname(__fileName);
+console.log(__dirName);
+
+app.set("views", path.join(__dirName, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use("/", userRouter);
+
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => {
+  console.log(`server Inakimbia kwenye porti ${PORT}`);
+});
